2f19e8adf8499369d55c2825b00f858db1a69173
Dhendy <dhendykur2@gmail.com>
2019-08-31 06:14:20 +0700

    a

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use('*', cors());
 app.use('/api', routes);
 
 app.use((req, res, next) => res.status(404).send(createError(404)));
